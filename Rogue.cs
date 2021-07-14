4ea7ae4cc925b9fa962a78445db90a71d176de88
Blagovest-Todorov <blagovest.stod@abv.bg>
2021-07-14 19:07:30 +0300

    Add the Project files

---
+++ Rogue.cs
@@ -0,0 +1,16 @@
+﻿namespace Raiding
+{
+    public class Rogue : BaseHero
+    {
+        private const int BasePower = 80;
+        public Rogue(string name) 
+            : base(name, BasePower)
+        {
+        }
+
+        public override string CastAbility()
+        {
+            return $"{nameof(Rogue)} - {this.Name} hit for {this.Power} damage";
+        }
+    }
+}
